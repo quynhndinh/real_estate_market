1239e89340a1b59583d56100407aa7938df75fa3
QuynhDinh <quynhndinh96@gmail.com>
2023-02-24 12:51:46 -0500

    added google map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Hero from './component/hero/hero.js';
+import GoogleMap from "./component/map/map.js";
 
 
 function App() {
   return (
     <div className="App">
       <Hero/>
+      <GoogleMap/>
     </div>
   );
 }
---
+++ src/component/map/map.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+
+const GoogleMap = () => {
+  let map;
+
+  const initMap = () => {
+    map = new window.google.maps.Map(document.getElementById("map"), {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8,
+    });
+  }
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDqhaZ9jDF5AIElywZEputMB9p3XTaAqIc&callback=initMap&v=weekly`;
+    script.defer = true;
+    script.addEventListener('load', initMap);
+    document.body.appendChild(script);
+  }, []);
+
+  return <div id="map" style={{ height: "950px" }}></div>;
+}
+
+export default GoogleMap;
